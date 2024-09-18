1ef2c9dd42b9d1755ce56d40fc964fd57e18abe7
Sajid Ali <thesajidali1997@gmail.com>
2024-09-18 15:15:38 +0500

    password-reset in development done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.default_url_options = { host:, protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
