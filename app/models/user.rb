dffb141f02b9dd86a9378ab1bac54683a9286d10
Sajid Ali <thesajidali1997@gmail.com>
2024-10-01 17:27:29 +0500

    status feed done

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,8 +72,6 @@ class User < ApplicationRecord
 
   def feed
     part_of_feed = 'relationships.follower_id = :id or microposts.user_id = :id'
-    following_ids = "SELECT followed_id FROM relationships
-                    WHERE follower_id = :user_id"
     Micropost.left_outer_joins(user: :followers)
              .where(part_of_feed, { id: }).distinct
              .includes(:user, image_attachment: :blob)
